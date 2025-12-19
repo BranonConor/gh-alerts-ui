4bd688d1fcee83fc4ad8e5138fee7bd88f6fa68a
branonconor <branonconor@github.com>
2025-12-19 15:42:56 -0800

    Added test-alert page layout

---
+++ src/app/(enterprises)/enterprises/[slug]/security/secret-scanning-alerts/test-alert/layout.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { Box } from "@primer/react";
+
+export default function SecretScanningAlertDetailLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "flex-start",
+                width: "100%",
+                height: "100%",
+            }}
+        >
+            <Box sx={{ maxWidth: "1280px", width: "100%", padding: 3 }}>
+                {children}
+            </Box>
+        </Box>
+    );
+}
